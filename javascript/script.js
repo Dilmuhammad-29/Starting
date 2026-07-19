3b8b813f6e87c60e28c0e7b858b18878a408b319
Dilmuhammad <lordpubg457@gmail.com>
2026-07-19 09:37:25 -0700

    new tabs

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,15 @@
-var searchColor = document.getElementsByClassName('search-input')
-var searchSubmit = document.getElementsByClassName('search-submit')
+const tabs = document.querySelectorAll(".text");
 
+tabs.forEach((tab) => {
+    tab.addEventListener("click", function () {
+
+        // Barcha tablardan active ni olib tashlash
+        tabs.forEach((item) => {
+            item.classList.remove("active");
+        });
+
+        // Bosilgan tabga active qo'shish
+        this.classList.add("active");
+
+    });
+});
